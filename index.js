049cbd7fc3a94bfbe3e60a3edb075e45f556f669
Emad <eal@my.harrisburgu.edu>
2022-08-09 18:13:01 -0700

    Add Prometheus & Grafana

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express= require('express');
 const mongoose= require('mongoose');
+const client = require('prom-client');
+
 
 const app=express();
 app.disable("x-powered-by");
@@ -24,7 +26,66 @@ app.use('/students',studentrouter)
 
 
 
+// ================ Promethues ==================
+
+//Create a Registry to register the metrics
+const register = new client.Registry();
+client.collectDefaultMetrics({
+    app: 'node-application-monitoring-app',
+    prefix: 'node_',
+    timeout: 10000,
+    gcDurationBuckets: [0.001, 0.01, 0.1, 1, 2, 5],
+    register
+});
+
+// Create a registry and pull default metrics
+
+app.get('/metrics', async (req, res) => {
+    // Start the HTTP request timer, saving a reference to the returned method
+    const end = httpRequestTimer.startTimer();
+    // Save reference to the path so we can record it when ending the timer
+    const route = req.route.path;
+      
+    res.setHeader('Content-Type', register.contentType);
+    res.send(await register.metrics());
+  
+    // End timer and add labels
+    end({ route, code: res.statusCode, method: req.method });
+  });
+
+// 
+app.get('/slow', async (req, res) => {
+    const end = httpRequestTimer.startTimer();
+    const route = req.route.path;
+    await createDelayHandler(req, res);
+    end({ route, code: res.statusCode, method: req.method });
+  });
+
+// Create a custom histogram metric
+const httpRequestTimer = new client.Histogram({
+    name: 'http_request_duration_seconds',
+    help: 'Duration of HTTP requests in seconds',
+    labelNames: ['method', 'route', 'code'],
+    buckets: [0.1, 0.3, 0.5, 0.7, 1, 3, 5, 7, 10] // 0.1 to 10 seconds
+  });
+
+// Register the histogram
+  register.registerMetric(httpRequestTimer);
+
+
+// Mock slow endpoint, waiting between 3 and 6 seconds to return a response
+const createDelayHandler = async (req, res) => {
+    if ((Math.floor(Math.random() * 100)) === 0) {
+      throw new Error('Internal Error')
+    }
+    // Generate number between 3-6, then delay by a factor of 1000 (miliseconds)
+    const delaySeconds = Math.floor(Math.random() * (6 - 3)) + 3
+    await new Promise(res => setTimeout(res, delaySeconds * 1000))
+    res.end('Slow url accessed!');
+  };
+
+//app.listen(9090, () => console.log('Server is running on http://localhost:8080, metrics are exposed on http://localhost:8080/metrics'));
+
 app.listen(port, () =>{
     console.log('Server started');
 })
-
